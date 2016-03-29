fa919c572bee0ba4fa3a15144c3380605631e372
Michael Bryan <michaelfbryan@gmail.com>
2016-03-29 22:21:40 +0800

    Added a troll message to the homepage for unauthenticated users

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, request, flash
+from flask import (render_template, session, redirect, url_for, request, 
+        flash, request)
 from flask.ext.login import current_user
+import requests
+from bs4 import BeautifulSoup
 
 from . import main
 # from .forms import NameForm
@@ -8,8 +11,27 @@ from .. import db
 from ..models import User 
 
 
-
 @main.route('/', methods=['GET'])
 def homepage():
-    return render_template('homepage.html')
+    if not current_user.is_authenticated:
+        ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        troll = lookup_ip(ip)
+        print(troll)
+    else:
+        troll = dict(success=False)
+
+    return render_template('homepage.html', troll=troll)
+
+
+def lookup_ip(ip):
+    if ip == '127.0.0.1':
+        return dict(success=False)
+
+    url = 'http://ip-api.com/json/{}'.format(ip)
+    headers = {
+        # 'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
+            }
+
+    r = requests.get(url, headers=headers)
+    return r.json()
 
